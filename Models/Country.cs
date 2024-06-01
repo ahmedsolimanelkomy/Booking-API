844710b46ff1a904f58b97ba1842bf7ed9ad860f
zienab alaa <zienabalaa442@gmail.com>
2024-06-01 22:19:24 +0300

    Add Car model car agency

---
+++ Models/Country.cs
@@ -0,0 +1,6 @@
+﻿namespace Booking_API.Models
+{
+    public class Country
+    {
+    }
+}
--- obj/Debug/net8.0/Booking API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Booking API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c999519721c34628ea059328d4c7d700303f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

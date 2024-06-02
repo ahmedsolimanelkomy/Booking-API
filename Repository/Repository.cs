9b9e56ab4520c475c9402633218a6a6e3820db93
ahmedsolimanelkomy <ahmedalkomy71@gmail.com>
2024-06-02 16:46:19 +0300

    Generic Repos v1

---
+++ Repository/Repository.cs
@@ -0,0 +1,87 @@
+﻿using Booking_API.Models;
+using Booking_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Booking_API.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly BookingContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(BookingContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string[]? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter, string[]? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(filter);
+        }
+
+        public IEnumerable<T> GetList(Expression<Func<T, bool>> filter, string[]? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet.Where(filter);
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

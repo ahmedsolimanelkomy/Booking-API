56b2b09c61de8c5ee2dc0bbd7a8f22bafc2209ec
ahmedsolimanelkomy <ahmedalkomy71@gmail.com>
2024-06-11 11:38:49 +0300

    mapping

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,7 +12,14 @@ namespace Booking_API.Mapping
             CreateMap<AdminDTO, ApplicationUser>().ReverseMap();
             CreateMap<HotelDTO, Hotel>();
             CreateMap<PassportDto, Passport>().ReverseMap();
-            CreateMap<RoomDTO, Room>();
+            CreateMap<RoomDTO, Room>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.AvailabilityStatus, opt => opt.MapFrom(src => src.AvailabilityStatus))
+            .ForMember(dest => dest.Capacity, opt => opt.MapFrom(src => src.Capacity))
+            .ForMember(dest => dest.View, opt => opt.MapFrom(src => src.View))
+            .ForMember(dest => dest.Hotel.Name, opt => opt.MapFrom(src => src.HotelName))
+            .ForMember(dest => dest.RoomType.Name, opt => opt.MapFrom(src => src.TypeName))
+            .ForMember(dest => dest.RoomType.PricePerNight, opt => opt.MapFrom(src => src.PricePerNight));
 
         }
     }

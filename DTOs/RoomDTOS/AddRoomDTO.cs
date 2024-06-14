94e2a8c3a64b028825b5b0d7f4b7ac0d203fd1e8
Anas <anasashrafelzayat@gmail.com>
2024-06-14 10:15:24 +0200

    test

--- DTOs/RoomDTOS/AddRoomDTO.cs
+++ DTOs/RoomDTOS/AddRoomDTO.cs
@@ -5,13 +5,11 @@ namespace Booking_API.DTOs.RoomDTOS
 {
     public class AddRoomDTO
     {
-<<<<<<< HEAD:DTOs/RoomDTO.cs
+
         
         public int? Id { get; set; }
 
-=======
-        public int Id { get; set; }
->>>>>>> 09a3c385fd57a783794dd72847543597556a32cb:DTOs/RoomDTOS/AddRoomDTO.cs
+
         public bool AvailabilityStatus { get; set; }
         public int Capacity { get; set; }
         public View? View { get; set; }
